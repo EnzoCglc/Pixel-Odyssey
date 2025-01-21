08a2a67384b94b3c5332b1dbbd6376e7456202d4
EnzoCglc <enzo.cougoulic@gmail.com>
2025-01-21 08:27:25 +0100

    MAP 3
    modif map

--- Scene/Item/Fruits/script/orange_jump.gd
+++ Scene/Item/Fruits/script/orange_jump.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var despawn: AnimatedSprite2D = $Area2D/Despawn
 @onready var animated_sprite_2d: AnimatedSprite2D = $Area2D/AnimatedSprite2D
 
-@export var Jump = -9000 #Valeur de base
+@export var Jump = -500 #Valeur de base
 @export var timer = 1
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -13,4 +13,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	despawn.play("default")
 	GameManager.jump_velocity = Jump
 	await get_tree().create_timer(timer).timeout
-	GameManager.jump_velocity = -550
+	GameManager.jump_velocity = -450
